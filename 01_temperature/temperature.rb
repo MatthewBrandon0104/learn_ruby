e169c98bda3de7a28c5f7f4902ee270b269e10f9
Matthew Brandon <matthewbrandon0104@gmail.com>
2018-06-07 16:28:19 -0700

    Exercise 02 Solved

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,21 @@
-#write your code here
+def ftoc(num)
+    num = (num - 32) * 5 / 9
+    return num.round
+end
+
+def ctof(num)
+    num = 1.8 * num + 32
+    if isInt(num) == true
+        return num.round
+    else
+        return num.round(1)
+    end
+end
+
+def isInt(arg)
+    if arg % 1 == 0
+        return true
+    else
+        return false
+    end
+end
