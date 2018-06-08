c4ce370658f6ea548a2617d5cc549d9f8e26504f
Matthew Brandon <matthewbrandon0104@gmail.com>
2018-06-07 22:55:46 -0700

    Calculator finished

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
-#write your code here
+def add(a,b)
+    return a + b
+end
+
+def subtract(a,b)
+    return a - b
+end
+
+def sum(array)
+    solution = 0
+    i = 0
+    while i < array.length
+        solution += array[i]
+        i += 1
+    end
+    return solution
+end
