71c3dbbc2efede881071504c7062e4a897af61bb
Matthew Brandon <matthewbrandon0104@gmail.com>
2018-06-07 23:26:28 -0700

    simons says WIP

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,22 @@
-#write your code here
+def echo(string)
+    return string
+end
+
+def shout(string)
+    return string.upcase
+end
+
+def repeat(string, num = 2)
+    string = string + " "
+    new_string = string * num
+    return new_string.chomp(" ")
+end
+
+def start_of_word(string, num)
+    return string[0..num - 1]
+end
+
+def first_word(string)
+    array = string.split
+    return array[0]
+end
