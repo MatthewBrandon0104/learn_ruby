136e092f588f26f4998383d251e8de58f585ebe8
Matthew Brandon <matthewbrandon0104@gmail.com>
2018-06-10 00:43:27 -0700

    titles Completed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
-# write your code here
+    def title=(string)
+        @title = string
+    end
+    
+    def title
+        string = @title
+        array = string.split()
+        array[0] = array[0].capitalize
+        
+        i = 1
+        while i < array.length
+            if array[i] == "a" || array[i] == "an" || array[i] == "and" || array[i] == "if" || array[i] == "the" || array[i] == "in" || array[i] == "of"
+                i += 1
+                next
+            else
+                array[i] = array[i].capitalize
+                i += 1
+            end
+        end
+        return array.join(" ")
+    end
 end
+
+
